7a004734013f857bb515989a33c54131b9c5f4b1
oberton31 <bertonoliver50@gmail.com>
2025-03-31 12:12:49 -0400

    validating integration of launch files

--- snaak_launch.bash
+++ snaak_launch.bash
@@ -8,8 +8,8 @@ echo "  ___/ / /|  / ___ |/ ___ |/ /| |  "
 echo " /____/_/ |_/_/  |_/_/  |_/_/ |_|  "
 
 if [ "$1" == "--start-frankapy" ]; then
-    ./home/snaak/Documents/frankapy/start_control_pc.sh -u snaak -i iam-doc
+    ./frankapy/bash_scripts/start_control_pc.sh -u snaak -i iam-doc
 fi
 
-source home/snaak/Documents/manipulation_ws/install/setup.bash
+source manipulation_ws/install/setup.bash
 ros2 launch snaak_state_machine snaak_state_machine_launch.py
